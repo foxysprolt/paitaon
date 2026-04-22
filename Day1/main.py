29749cf30a33b9214e0c15bca1f5a4d7d23e618a
Foxysprolt <945534743rafa@gmail.com>
2026-04-22 20:56:16 -0300

    day1

---
+++ Day1/main.py
@@ -0,0 +1,10 @@
+#primeiro vou pegar as informações importantes e guardas elas como variaveis
+celsius = float(input("Digite os Graus Celsius para transforma-los em Fahrenheit: "))
+Fahrenheit = float(input("Digite os Graus Fahrenheit para transforma-los em Celsius: "))
+
+dolar = float(input("Digite os dolares para transforma-los em reais: "))
+real = float(input("Digite os reais  para transforma-los em dolares: "))
+
+metro = float(input("Digite os metros para transforma-los em centimetros: "))
+centimetros = float(input("Digite os centimetros para transforma-los em metros: "))
+
