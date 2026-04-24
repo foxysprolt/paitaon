79651ca7e3d2d695e688fdb50da8386e5e3875d1
R4F43L <945534743rafa@gmail.com>
2026-04-23 22:20:30 -0300

    Add IMC calculation function in main.py
    ainda to fazendo calma

---
+++ day2/main.py
@@ -0,0 +1,9 @@
+def Calc_IMC():
+    If:
+        Return "Você está no grau de obesidade 2"
+    
+print("Bem vindo a calculadora de IMC")
+alt = float(input("Me diga sua altura: "))
+pes = float(input("Me diga o seu peso: "))
+
+print(Calc_IMC())
